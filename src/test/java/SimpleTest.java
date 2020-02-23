5ccad5ebfe5d0ecd6906901aa2a014ba6bc8b92e
yvzydn <yavuzaydinus@gmail.com>
2020-02-23 12:19:28 -0500

    Creating a new repo›

---
+++ src/test/java/SimpleTest.java
@@ -0,0 +1,10 @@
+import org.testng.annotations.Test;
+
+public class SimpleTest {
+    @Test
+    public void passingTest(){
+        System.out.println("This test will always pass!");
+    }
+
+
+}
